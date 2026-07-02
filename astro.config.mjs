a25aca8909cda88318403e2f66385daac18cda2b
Angel <angello3711@outlook.com>
2026-07-02 04:24:01 -0500

    fix: quitar base incorrecto para sitio de usuario

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ const isProduction = process.env.NODE_ENV === "production";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://angellontiveros.github.io",
-	base: isProduction ? "/Portafolio" : "/",
+	base: "/",
 	integrations: [tailwind()],
 });
